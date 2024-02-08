1ba110136210e31de0bf7722292ce7f7b71db732
Edson Aju <avataredson@gmail.com>
2024-02-07 19:41:12 -0600

    cambio database online

--- config/databaseOnline.js
+++ config/databaseOnline.js
@@ -1,15 +1,23 @@
 import mysql from 'mysql2/promise'; // Importar la versión 'promise' de mysql2
+import dotenv from 'dotenv';
 
+const DEFAULT_CONFIG = {
+  host: 'localhost',
+      user: 'admin',
+      port: 3306,
+      password: '123456',
+      database: 'redddb'
+};
+
+dotenv.config(); 
+
+// Verifica si la variable de entorno DATABASE_URL está definida, de lo contrario, usa la configuración predeterminada
+const connectionString =  process.env.DATABASE_URL ? process.env.DATABASE_URL : DEFAULT_CONFIG;
+console.log('CONN',connectionString,process.env.DATABASE_URL  )
 // Función para conectar a la base de datos MySQL
 const connectToDatabase = async () => {
   try {
-    const connection = await mysql.createConnection({
-      host: 'mysql-e785997-jparielsan-7817.a.aivencloud.com',
-      user: 'avnadmin',
-      port: 20587,
-      password: 'AVNS_xkywKtsropPDgyFcFq3',
-      database: 'redddb'
-    });
+    const connection = await mysql.createConnection(connectionString);
     console.log('Conexión exitosa a la base de datos.');
     return connection;
   } catch (error) {
