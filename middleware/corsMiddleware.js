3cfc73023272baa90b0d967ed382e41e1fa2ced5
Edson Aju <eaju@plusti.com>
2024-02-07 22:12:10 -0600

    cors update v 1.0.2

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:1234',
+    'http://localhost:4200',
     'https://pruebas.com',
     'https://test.dev'
   ]
